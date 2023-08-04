f4b436c7da40c9d1f1df081ab2cabd503443c585
Bii <dev.nguyentantai@gmail.com>
2023-08-04 12:28:19 +0700

    Add New Project

---
+++ Readme.txt
@@ -0,0 +1,4 @@
+Thanks for watching this website!
+
+ Name: ADMIN's Website
+ URL: https://dev-ngtantai.github.io/Admin
---
+++ knock/users.js
@@ -0,0 +1,19 @@
+var admins = [
+    {
+        name: "admin",
+        password: "admin",
+        url: "NgTanTai.html"
+    },
+    {
+        name: "NgTanTai",
+        password: "TaiKhoan",
+        url: "Account.html"
+    },
+    {
+        name: "NgTanTai",
+        password: "BangCap",
+        url: "Learning.html"
+    },
+   
+   
+]
